71f6895184c77f3d9f885487dded0c93135f58f9
KhaledOdehh <[200~khaled.awd.odeh@gmail.com>
2024-06-11 23:49:57 +0300

    endpoint is done /expect presenters edit 2

--- backend/controllers/presenters.js
+++ backend/controllers/presenters.js
@@ -2,50 +2,29 @@ const presentersModel = require("../models/presenters");
 const jobModel = require("../models/job");
 
 const createApplyForJob = (req, res) => {
-    const jobId = req.params.id;
-    const userId = req.token.userId;
-console.log(jobId);
-console.log(userId);
-
-presentersModel.findOne({ job: jobId })
-        .then((job) => {
-            if (!job) {
-               return  res.status(404).json({
-                    success: false,
-                    message: `Job not found`,
-                });
-            } 
-            
-            const jobApplied = new presentersModel({
-               job,
-               seeker, 
-            });
-
-             jobApplied.save()
-                .then((savedJob) => {console.log(savedJob);
-                     presentersModel.findByIdAndUpdate(
-                       
-                       {_id:savedJob._id},
-                        { $push: { seeker: userId } },
-                        { new: true }
-                    ).then((result)=>{
-                       res.json(result) 
-                    }).catch((err)=>{console.log(err);
-                    })
-                
-                }).catch((err)=>{
-console.log(err);
-                })
-         }) 
-       
-      .catch((err) => {
-          res.status(500).json({
-              success: false,
-              message: `Server Error`,
-              err: err.message,
+  const job = req.params.id;
+  const seeker = req.token.userId;
+  presentersModel.findOne({ job: job })
+  .then((exist) => {
+      if (!exist) {
+        const jobApplied = new presentersModel({
+            job,
+            seeker,
           });
-      }); 
         
+          jobApplied
+            .save()
+            .then((savedJob) => {
+              console.log(savedJob);
+             
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+}  
+
+}).catch((err)=>{console.log(err);}) 
+  
 };
 
 module.exports = { createApplyForJob };
